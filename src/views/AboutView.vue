a78e8679138f30f2783773442382b3a4a110037e
okeowo-mike <okeowom7@gmail.com>
2025-03-03 23:51:39 +0100

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque consectetur repudiandae
+      placeat exercitationem enim aspernatur ut quaerat, soluta suscipit libero tenetur nihil illum
+      veniam quibusdam odio accusamus iste corrupti dolorem!
+    </p>
+    <br />
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea possimus alias voluptates. Unde,
+      distinctio! Obcaecati accusamus quasi laborum corporis ipsa, dolores harum architecto minima
+      nostrum sint a quaerat placeat nihil doloribus odio optio nam saepe, sunt commodi voluptatibus
+      ea similique nisi consequatur. Maiores?
+    </p>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>This is My New Construction Site</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores voluptate facilis
+      distinctio laudantium repudiandae, sapiente accusamus vitae nostrum fuga odio eveniet soluta
+      reiciendis aut eius inventore cum, quia ea in veritatis natus ab repellat! Necessitatibus odit
+      cumque ullam sint obcaecati beatae eum non, vitae, veritatis asperiores suscipit quis
+      reiciendis.
+    </p>
+  </div>
+</template>

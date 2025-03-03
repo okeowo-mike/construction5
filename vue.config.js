a1243eb5be7b24de03dd15d45c366fe838ecee58
okeowo-mike <okeowom7@gmail.com>
2025-03-04 00:10:16 +0100

    Corrected publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
 });
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-project/'  // Replace with your repository name if deploying to a subdirectory
-    : '/'
-}
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/construction5/" // Replace with your repository name if deploying to a subdirectory
+      : "/",
+};
